de68b998862f440b36996410b12a2c51dac2696b
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2020-12-29 22:59:31 -0600

    Se consume el end point /clientes usando el useEffect y se guarda en el state mediente el hook useState y queda listo para ser mostrado

--- src/components/clientes/Clientes.js
+++ src/components/clientes/Clientes.js
@@ -1,7 +1,44 @@
+import React, {useEffect, useState, Fragment} from 'react';
+
+// importamos clienteAxios
+import clienteAxios from '../../config/axios';
 
 function Clientes() {
+
+	/**  Definimos el state */
+	// clientes es el state
+	// guardarClientes es la funcion que modifica el state
+	const [clientes, guardarClientes] = useState([])
+
+	// Query a la API
+	const consultarAPI = async () => {
+		// console.log('Consultando...');
+		const clientesConsulta = await clienteAxios.get('/clientes');
+
+		// para testear lo que retora la API
+		// console.log(clientesConsulta.data);
+
+		// almacenmos la data en el state
+		guardarClientes(clientesConsulta.data);
+
+
+	}
+
+	//usamos el hook useEffect
+	useEffect( () => {
+		consultarAPI();
+	}, []);
+
    return (
-		<h2>Clientes</h2> 
+		<Fragment>
+			<h2>Clientes</h2>
+
+			<ul className="listado-clientes">
+				{clientes.map(cliente => {
+					console.log(cliente);
+				})}
+			</ul>
+		</Fragment>
 	)
 }
 
